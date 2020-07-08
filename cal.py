24e6bc8671bb7352afc0f710d96fd155c2a733f6
Aakash <64589033+Aakash-online@users.noreply.github.com>
2020-07-09 02:12:35 +0530

    Update cal.py

--- cal.py
+++ cal.py
@@ -6,7 +6,6 @@ root.title("CALCULATOR")
 #Frame for Text screen
 textF = Frame(root)
 textF.pack(fill="both")
-
 #input-output variable which will be showed o screen as input (question) and output (Answer)
 io = Variable()
 
@@ -33,10 +32,18 @@ def click(event):
         else :
             #  Indeterminate forms can cause Error so have to display error
             try :
+                #getting whole calculation part from the screen as input-terms
+                input_terms = str(io.get())
+
+                #Python use "*" as symbol of multiplication so we have to replace "x" with it and we achieve final-string
+                finalstring= input_terms.replace("x", "*")
+
                 # "eval()" function evalutes the string input and gives answer
-                value = eval (io.get ( ))
-                #Displaying the result
-                io.set(value)
+                value = eval (finalstring)
+
+                # Displaying the result
+                io.set (value)
+
 
             except Exception as e :
                 print (e)
@@ -67,58 +74,57 @@ butF.columnconfigure (3, {'minsize' : 30})
 
                       #BUttons Which will be displayed in second frame below the screen
 
-onebutton = Button (butF, text="1", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+onebutton = Button (butF, text="1", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 onebutton.grid (row=1, column=0,pady=10,padx=10)
 onebutton.bind("<Button>" , click )
-fourebutton = Button (butF, text="4", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+fourebutton = Button (butF, text="4", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 fourebutton.grid (row=2, column=0,pady=10,padx=10)
 fourebutton.bind("<Button>" , click )
-sevenbutton = Button (butF, text="7", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+sevenbutton = Button (butF, text="7", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 sevenbutton.grid (row=3, column=0,pady=10,padx=10)
 sevenbutton.bind("<Button>" , click )
-dotbutton = Button (butF, text=".", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+dotbutton = Button (butF, text=".", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 dotbutton.grid (row=4, column=0,pady=10,padx=10)
 dotbutton.bind("<Button>" , click )
-twobutton = Button (butF, text="2", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+twobutton = Button (butF, text="2", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 twobutton.grid (row=1, column=1,pady=10,padx=10)
 twobutton.bind("<Button>" , click )
-fivebutton = Button (butF, text="5", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+fivebutton = Button (butF, text="5", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 fivebutton.grid (row=2, column=1,pady=10,padx=10)
 fivebutton.bind("<Button>" , click )
-eightbutton = Button (butF, text="8", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+eightbutton = Button (butF, text="8", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 eightbutton.grid (row=3, column=1,pady=10,padx=10)
 eightbutton.bind("<Button>" , click )
-zerobutton = Button (butF, text="0", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+zerobutton = Button (butF, text="0", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 zerobutton.grid (row=4, column=1,pady=10,padx=10)
 zerobutton.bind("<Button>" , click )
-threebutton = Button (butF, text="3", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+threebutton = Button (butF, text="3", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 threebutton.grid (row=1, column=2,pady=10,padx=10)
 threebutton.bind("<Button>" , click )
-sixbutton = Button (butF, text="6", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+sixbutton = Button (butF, text="6", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 sixbutton.grid (row=2, column=2,pady=10,padx=10)
 sixbutton.bind("<Button>" , click )
-ninebutton = Button (butF, text="9", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+ninebutton = Button (butF, text="9", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 ninebutton.grid (row=3, column=2,pady=10,padx=10)
 ninebutton.bind("<Button>" , click )
-divbutton = Button (butF, text="/", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+divbutton = Button (butF, text="/", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 divbutton.grid (row=4, column=2,pady=10,padx=10)
 divbutton.bind("<Button>" , click )
-acbutton = Button (butF, text="AC", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+acbutton = Button (butF, text="AC", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 acbutton.grid (row=0, column=3,pady=10,padx=10)
 acbutton.bind("<Button>" , click )
-plusbutton = Button (butF, text="+", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+plusbutton = Button (butF, text="+", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 plusbutton.grid (row=1, column=3,pady=10,padx=10)
 plusbutton.bind("<Button>" , click )
-minbutton = Button (butF, text="-", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+minbutton = Button (butF, text="-", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 minbutton.grid (row=2, column=3,pady=10,padx=10)
 minbutton.bind("<Button>" , click )
-mulbutton = Button (butF, text="x", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+mulbutton = Button (butF, text="x", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 mulbutton.grid (row=3, column=3,pady=10,padx=10)
 mulbutton.bind("<Button>" , click )
-equbutton = Button (butF, text="=", fg="yellow",height = 1, width = 5, font = "Helvetica 10 bold")
+equbutton = Button (butF, text="=", fg="red",height = 1, width = 5, font = "Helvetica 10 bold")
 equbutton.grid (row=4, column=3,pady=10,padx=10)
 equbutton.bind("<Button>" , click )
-
 
 root.mainloop()
 
